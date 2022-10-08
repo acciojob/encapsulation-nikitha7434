f9ede053891a25bbb6a58b711f60860adf36e0af
Nikitha <nikithamashetti434@gmail.com>
2022-10-08 20:51:10 +0530

    add code git

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
-public class Main {
-  
+public class Main{
+    public static void main(String[] args) {
+        RWOnly obj =new RWOnly();
+//obj.name="nikitha";
+//java: name has private access in com.driver.RWOnly
+obj.setName("nikitha");
+System.out.print(obj.getName());
+    }
 }
